a96b8d55c4490e6730b98fb4d3e74ae2268702fa
Denis Smirnov <upccore@list.ru>
2026-05-23 11:47:26 +0300

    add EventsPaginator for cursor-based pagination

---
+++ src/paginator.py
@@ -0,0 +1,31 @@
+class EventsPaginator:
+    def __init__(self, client):
+        self.client = client
+
+    def __aiter__(self):
+        self._next_url = None
+        self._first_page = True
+        self._current_page_events = []
+        self._current_index = 0
+        return self
+
+    async def __anext__(self):
+        if self._current_index >= len(self._current_page_events):
+            if self._first_page:
+                response = await self.client.get_events("2000-01-01")
+                self._first_page = False
+            elif self._next_url:
+                response = await self.client.get_events("", self._next_url)
+            else:
+                raise StopAsyncIteration
+
+            self._current_page_events = response["results"]
+            self._next_url = response.get("next")
+            self._current_index = 0
+
+            if not self._current_page_events:
+                raise StopAsyncIteration
+
+        event = self._current_page_events[self._current_index]
+        self._current_index += 1
+        return event
